5a9d90f2ad16e1c8035543ac11676f39b6dbe80b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-23 11:43:10 -0700

    Agregar archivos de proyecto.

---
+++ Contornos/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Contornos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarImagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contornoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectarFormasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.contornoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(969, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarImagenToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // cargarImagenToolStripMenuItem
+            // 
+            this.cargarImagenToolStripMenuItem.Name = "cargarImagenToolStripMenuItem";
+            this.cargarImagenToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.cargarImagenToolStripMenuItem.Text = "Cargar Imagen ";
+            this.cargarImagenToolStripMenuItem.Click += new System.EventHandler(this.cargarImagenToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // contornoToolStripMenuItem
+            // 
+            this.contornoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detectarToolStripMenuItem,
+            this.detectarFormasToolStripMenuItem});
+            this.contornoToolStripMenuItem.Name = "contornoToolStripMenuItem";
+            this.contornoToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.contornoToolStripMenuItem.Text = "Contorno";
+            // 
+            // detectarToolStripMenuItem
+            // 
+            this.detectarToolStripMenuItem.Name = "detectarToolStripMenuItem";
+            this.detectarToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.detectarToolStripMenuItem.Text = "Detectar";
+            this.detectarToolStripMenuItem.Click += new System.EventHandler(this.detectarToolStripMenuItem_Click);
+            // 
+            // detectarFormasToolStripMenuItem
+            // 
+            this.detectarFormasToolStripMenuItem.Name = "detectarFormasToolStripMenuItem";
+            this.detectarFormasToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.detectarFormasToolStripMenuItem.Text = "Detectar formas";
+            this.detectarFormasToolStripMenuItem.Click += new System.EventHandler(this.DetectarFormasToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(556, 522);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(408, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(549, 522);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 584);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarImagenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem contornoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectarFormasToolStripMenuItem;
+    }
+}
+
---
+++ Contornos/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace Contornos
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> _imgInput;
+        Image<Bgr, byte> _imgGray;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cargarImagenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                _imgInput = new Image<Bgr, byte>(ofd.FileName);
+                pictureBox1.Image = _imgInput.Bitmap;
+            }
+        }
+
+        private void detectarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Binarizacion
+            Image<Gray, byte> _imgOutput = _imgInput.Convert<Gray, byte>().ThresholdBinary(new Gray(200), new Gray(255));
+            Emgu.CV.Util.VectorOfVectorOfPoint contours = new Emgu.CV.Util.VectorOfVectorOfPoint();
+            Mat hier = new Mat();
+
+            //Encontrat los contornos en la imagen
+            CvInvoke.FindContours(_imgOutput, contours, hier, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.DrawContours(_imgOutput, contours, -1, new MCvScalar(255, 0, 0));
+
+            pictureBox2.Image = _imgOutput.Bitmap;
+        }
+
+        private void DetectarFormasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_imgInput == null)
+            {
+                return;
+            }
+
+            try
+            {
+                //Suaviza la imagen y aplica binarizacion y almacena en variable temporal
+                var temp = _imgInput.SmoothGaussian(5).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(230), new Gray(255));
+
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat n = new Mat();
+
+                //Aplicar funcion FindCountour para detectar los contornos de las figuras.
+                CvInvoke.FindContours(temp, contours, n, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+
+                for (int i=0; i < contours.Size; i++)
+                {
+                    double perimetro = CvInvoke.ArcLength(contours[i], true);
+                    VectorOfPoint aprox = new VectorOfPoint();
+                    //Deteccion de curvas a una presicion indicada eje(0.04)
+                    CvInvoke.ApproxPolyDP(contours[i], aprox, 0.04 * perimetro, true);
+
+                    CvInvoke.DrawContours(_imgInput, contours, i, new MCvScalar(0, 0, 255), 2);
+
+                    //Encontrar centro de la figura
+                    var momentos = CvInvoke.Moments(contours[i]);
+                    int x = (int)(momentos.M10 / momentos.M00);
+                    int y = (int)(momentos.M01 / momentos.M00);  
+
+                    if (aprox.Size == 3)
+                    {
+
+                        CvInvoke.PutText(_imgInput, "triangulo", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+
+                    }
+
+                    if (aprox.Size == 4)
+                    {
+
+                        Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+
+                        double ar = (double)rect.Width / rect.Height;
+
+                        if (ar >= 0.95 && ar <= 1.05)
+                        {
+                            CvInvoke.PutText(_imgInput, "Cuadrado", new Point(x, y), 
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+
+
+                        }
+                        else
+                        {
+                            CvInvoke.PutText(_imgInput, "Rectangulo", new Point(x, y), 
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                        }
+
+                    }
+
+                    pictureBox2.Image = _imgInput.Bitmap;
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
